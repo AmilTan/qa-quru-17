06333172797fbe6834516fd01a77ea135d732696
AmilTan <109027411+AmilTan@users.noreply.github.com>
2024-01-31 18:55:33 +0300

    -static metod move2

--- src/test/java/demoqa/FormWithObjects/Pages/RegistrationWithObjectTests.java
+++ src/test/java/demoqa/FormWithObjects/Pages/RegistrationWithObjectTests.java
@@ -26,7 +26,7 @@ public class RegistrationWithObjectTests extends TestBase {
     @Link(value = "Testing", url = "https://demoqa.com/automation-practice-form")
     @DisplayName("Позитивный тест: формирование валидной заявки")
     void fillFormTest() throws InterruptedException {
-        registrationPage.openPage()
+        new RegistrationPage().openPage()
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(email)
@@ -64,7 +64,7 @@ public class RegistrationWithObjectTests extends TestBase {
     @Link(value = "Testing", url = "https://demoqa.com/automation-practice-form")
     @DisplayName("Негативный тест: не введен номер телефона")
     void negativeFillFormTest() throws InterruptedException {
-                registrationPage.openPage()
+        new RegistrationPage().openPage()
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(email)
